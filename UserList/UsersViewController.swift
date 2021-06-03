271180a4a89972916f79d16e11863252b1cc8c68
Yaz Burrell <yburrell@zillion.com>
2021-06-03 13:53:56 -0400

    building network call

--- UserList/UsersViewController.swift
+++ UserList/UsersViewController.swift
@@ -14,6 +14,41 @@ class UsersViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello")
+        getUsers()
+    }
+    
+    func getUsers(){
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                print(httpResponse.statusCode)
+            }
+            
+            if let error = error {
+                print(error)
+            } else if let data = data {
+                print(data)
+                
+                let dataString = String(data: data, encoding: .utf8)
+                //print(dataString)
+                
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                    
+                } catch let err {
+                print(err)
+                }
+            }
+        }
+        
+        dataTask.resume()
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
